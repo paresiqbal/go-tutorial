842ae9cbc4c8cd0e8f03e77f87595287e294b8c0
paresiqbal <thecrowdel@gmail.com>
2023-10-30 22:03:27 +0700

    Type data alias

--- convers.go
+++ convers.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var value32 int32 = 100000
-	var value64 int64 = int64(value32)
-	var value8 int8 = int8(value64)
+// func main() {
+// 	var value32 int32 = 100000
+// 	var value64 int64 = int64(value32)
+// 	var value8 int8 = int8(value64)
 
-	fmt.Println(value32)
-	fmt.Println(value64)
-	fmt.Println(value8)
+// 	fmt.Println(value32)
+// 	fmt.Println(value64)
+// 	fmt.Println(value8)
 
-	var name = "Pares"
-	var p byte = name[0]
-	var eString string = string(p)
+// 	var name = "Pares"
+// 	var p byte = name[0]
+// 	var eString string = string(p)
 
-	fmt.Println(name)
-	fmt.Println(eString)
-}
+// 	fmt.Println(name)
+// 	fmt.Println(eString)
+// }
---
+++ tyoe-dec.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	type NoKTP string
+
+	var noKTPPares NoKTP = "87216482678"
+	fmt.Println(noKTPPares)
+}
