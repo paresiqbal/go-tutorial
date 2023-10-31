3d8d35711d4dadc55769386f9dc486054174631d
paresiqbal <thecrowdel@gmail.com>
2023-10-31 22:42:08 +0700

    sdas

--- math-operation.go
+++ math-operation.go
@@ -1,19 +1,19 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	var result = 10 + 10
-	fmt.Println(result)
+// func main() {
+// 	var result = 10 + 10
+// 	fmt.Println(result)
 
-	var a = 10
-	var b = 10
-	var c = a * b
-	fmt.Println(c)
+// 	var a = 10
+// 	var b = 10
+// 	var c = a * b
+// 	fmt.Println(c)
 
-	var i = 10
-	i += 5 // i + 5 | 10 + 5 (auhmented asigment)
-	i++    // ++ artinya +1
-	i--    // -- artinya -1
-	fmt.Println(i)
-}
+// 	var i = 10
+// 	i += 5 // i + 5 | 10 + 5 (auhmented asigment)
+// 	i++    // ++ artinya +1
+// 	i--    // -- artinya -1
+// 	fmt.Println(i)
+// }
