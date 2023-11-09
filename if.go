ca2982c533d048132be2efeaea6dec06e3629468
paresiqbal <thecrowdel@gmail.com>
2023-11-09 11:21:05 +0700

    If

---
+++ if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Pares"
+
+	if name == "Iqbal" {
+		fmt.Println("Benar")
+	} else {
+		fmt.Println("Salah")
+	}
+}
--- map.go
+++ map.go
@@ -1,22 +1,22 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	person := map[string]string{
-		"name":    "Pares",
-		"address": "Curup",
-	}
+// func main() {
+// 	person := map[string]string{
+// 		"name":    "Pares",
+// 		"address": "Curup",
+// 	}
 
-	fmt.Println(person)
+// 	fmt.Println(person)
 
-	book := make(map[string]string)
-	book["title"] = "Buku golang"
-	book["author"] = "Pares"
-	book["ups"] = "ups"
+// 	book := make(map[string]string)
+// 	book["title"] = "Buku golang"
+// 	book["author"] = "Pares"
+// 	book["ups"] = "ups"
 
-	fmt.Println(book)
-	delete(book, "ups")
+// 	fmt.Println(book)
+// 	delete(book, "ups")
 
-	fmt.Println(book)
-}
+// 	fmt.Println(book)
+// }
