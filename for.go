f095872aad24001bd9fd92beb0a246edd729bf55
paresiqbal <thecrowdel@gmail.com>
2023-11-09 13:02:21 +0700

    break

--- for.go
+++ for.go
@@ -1,25 +1,25 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	for counter := 1; counter <= 20; counter++ {
-		fmt.Println("Perulangan ke", counter)
+// func main() {
+// 	for counter := 1; counter <= 20; counter++ {
+// 		fmt.Println("Perulangan ke", counter)
 
-	}
+// 	}
 
-	fmt.Println("Selesai")
+// 	fmt.Println("Selesai")
 
-	names := []string{"Pares", "Iqbal", "Prastowo"}
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
-	}
+// 	names := []string{"Pares", "Iqbal", "Prastowo"}
+// 	for i := 0; i < len(names); i++ {
+// 		fmt.Println(names[i])
+// 	}
 
-	for index, name := range names {
-		fmt.Println("Index", index, "=", name)
-	}
+// 	for index, name := range names {
+// 		fmt.Println("Index", index, "=", name)
+// 	}
 
-	for _, name := range names {
-		fmt.Println(name)
-	}
-}
+// 	for _, name := range names {
+// 		fmt.Println(name)
+// 	}
+// }
