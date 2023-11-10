073bd6a189b458ece72dc0f55e1fb296b9a9399e
paresiqbal <thecrowdel@gmail.com>
2023-11-10 19:20:57 +0700

    asdasd

---
+++ function-parameter.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sayHello(name string, filter func(string) string) {
+	flteredName := filter(name)
+	fmt.Println("Hello", flteredName)
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "***"
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHello("Pares", spamFilter)
+
+	filter := spamFilter
+	sayHello("Anjing", filter)
+}
--- function-value.go
+++ function-value.go
@@ -1,13 +1,13 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func getNews(name string) string {
-	return "New News" + name
-}
+// func getNews(name string) string {
+// 	return "New News" + name
+// }
 
-func main() {
-	newNews := getNews
+// func main() {
+// 	newNews := getNews
 
-	fmt.Println(newNews("AAA"))
-}
+// 	fmt.Println(newNews("AAA"))
+// }
