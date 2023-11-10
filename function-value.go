36ce4ed8c72b9167a73705018eb577b67177dba1
paresiqbal <thecrowdel@gmail.com>
2023-11-10 19:10:39 +0700

    value

---
+++ function-value.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func getNews(name string) string {
+	return "New News" + name
+}
+
+func main() {
+	newNews := getNews
+
+	fmt.Println(newNews("AAA"))
+}
--- variadic-fucntion.go
+++ variadic-fucntion.go
@@ -1,19 +1,19 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func sumAll(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
+// func sumAll(numbers ...int) int {
+// 	total := 0
+// 	for _, number := range numbers {
+// 		total += number
+// 	}
 
-	return total
-}
+// 	return total
+// }
 
-func main() {
-	fmt.Println(sumAll(10, 10, 10))
+// func main() {
+// 	fmt.Println(sumAll(10, 10, 10))
 
-	numbers := []int{10, 10, 10}
-	fmt.Println(sumAll(numbers...))
-}
+// 	numbers := []int{10, 10, 10}
+// 	fmt.Println(sumAll(numbers...))
+// }
