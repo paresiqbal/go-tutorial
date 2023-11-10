a8dc50e163a5e6891b7699946d4601f99aaabad3
paresiqbal <thecrowdel@gmail.com>
2023-11-10 21:26:10 +0700

    asdas

--- anonymous.go
+++ anonymous.go
@@ -1,25 +1,25 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Blacklist func(string) bool
+// type Blacklist func(string) bool
 
-func registerUser(name string, blacklist Blacklist) {
-	if blacklist(name) {
-		fmt.Println("You are blocked", name)
-	} else {
-		fmt.Println("Welcome", name)
-	}
-}
+// func registerUser(name string, blacklist Blacklist) {
+// 	if blacklist(name) {
+// 		fmt.Println("You are blocked", name)
+// 	} else {
+// 		fmt.Println("Welcome", name)
+// 	}
+// }
 
-func main() {
-	blacklist := func(name string) bool {
-		return name == "Pares"
-	}
+// func main() {
+// 	blacklist := func(name string) bool {
+// 		return name == "Pares"
+// 	}
 
-	registerUser("Iqbal", blacklist)
-	// Anonymous function
-	registerUser("Pares", func(name string) bool {
-		return name == "Pares"
-	})
-}
+// 	registerUser("Iqbal", blacklist)
+// 	// Anonymous function
+// 	registerUser("Pares", func(name string) bool {
+// 		return name == "Pares"
+// 	})
+// }
