4ce1f349bcd2f409064eb57fdac42ca8d7fd45a4
paresiqbal <thecrowdel@gmail.com>
2023-11-13 23:35:49 +0700

    Add value to artibute

--- struct.go
+++ struct.go
@@ -1,11 +1,19 @@
 package main
 
+import "fmt"
+
 type Customer struct {
 	Name, Adress string
 	Age          int
-	maried       bool
+	Maried       bool
 }
 
 func main() {
+	var pares Customer
+	pares.Name = "Pares Iqbal"
+	pares.Adress = "Bengkulu"
+	pares.Age = 23
+	pares.Maried = false
 
+	fmt.Println(pares.Name)
 }
