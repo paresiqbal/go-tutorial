dd1bc62fbef870a598e34e272564b062e9446018
paresiqbal <thecrowdel@gmail.com>
2023-11-14 22:01:44 +0700

    new

--- asterik.go
+++ asterik.go
@@ -1,18 +1,18 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Adress struct {
-	City, Provice, Country string
-}
+// type Adress struct {
+// 	City, Provice, Country string
+// }
 
-func main() {
-	var address1 Adress = Adress{"Curup", "Bengkulu", "Indonesia"}
-	var address2 *Adress = &address1 // value dari adress1 di copy ke address2
+// func main() {
+// 	var address1 Adress = Adress{"Curup", "Bengkulu", "Indonesia"}
+// 	var address2 *Adress = &address1 // value dari adress1 di copy ke address2
 
-	address2.City = "Bandung"
+// 	address2.City = "Bandung"
 
-	*address2 = Adress{"Jakarta", "DKI Jakarta", "Indonesia"}
-	fmt.Println(address1)
-	fmt.Println(address2)
-}
+// 	*address2 = Adress{"Jakarta", "DKI Jakarta", "Indonesia"}
+// 	fmt.Println(address1)
+// 	fmt.Println(address2)
+// }
---
+++ new.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Adress struct {
+	City, Province, Country string
+}
+
+func main() {
+	var alamat1 *Adress = new(Adress)
+	var alamat2 *Adress = alamat1
+
+	alamat2.Country = "Indonesia"
+
+	fmt.Println(alamat1)
+	fmt.Println(alamat2)
+}
