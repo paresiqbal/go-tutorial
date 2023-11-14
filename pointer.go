4764e5b8d8f361282d45660c01e0a9db727cbf8c
paresiqbal <thecrowdel@gmail.com>
2023-11-14 21:51:02 +0700

    Ponter

--- pointer.go
+++ pointer.go
@@ -1,16 +1,16 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type Adress struct {
-	City, Province, Country string
-}
+// type Adress struct {
+// 	City, Province, Country string
+// }
 
-func main() {
-	address1 := Adress{"Curup", "Bengkulu", "Indonesia"}
-	address2 := address1 // value dari adress1 di copy ke address2
+// func main() {
+// 	address1 := Adress{"Curup", "Bengkulu", "Indonesia"}
+// 	address2 := &address1 // value dari adress1 di copy ke address2
 
-	address2.City = "Bandung"
-	fmt.Println(address1)
-	fmt.Println(address2)
-}
+// 	address2.City = "Bandung"
+// 	fmt.Println(address1)
+// 	fmt.Println(address2)
+// }
